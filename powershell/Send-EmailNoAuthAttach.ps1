892f1fe522f89d7c6d84a8e07cf6419afffe940d
Matt Milliman <matt.milliman@hyland.com>
2024-04-30 16:53:52 -0400

    Add PS script to email attachment

---
+++ powershell/Send-EmailNoAuthAttach.ps1
@@ -0,0 +1,12 @@
+﻿#Create test file in Windows
+#fsutil file createnew test.txt 30720000
+
+$smtpServer = "<SMTP-SERVER>"
+$smtpPort = "25"
+$emailTo = "<TO-ADDRESS>"
+$emailFrom = "<FROM-ADDRESS>"
+$emailSubject = "<SUBJECT>"
+$emailBody = "<MESSAGE>"
+$attachment = "<ATTACHMENT>"
+
+Send-MailMessage -To $emailTo -Subject $emailSubject -BodyAsHtml:$true -Body $emailBody -From $emailFrom -Attachment $attachment -SmtpServer $smtpServer -Port $smtpPort
